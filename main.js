ccff2ebe247269d210dc93d5ec75ae81ab35c0ce
Mahe12347 <75433421+Mahe12347@users.noreply.github.com>
2021-06-15 17:47:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 img="";
+status="";
 function setup()
 {
 canvas=createCanvas(640,420);
 canvas.center();
+objectDetector('cocossd',modelLoaded);
+document.getElementById("status").innerHTML="status:Detecting objects";
+
 }
 function preload(){
     img=loadImage('dog_cat.jpg');
@@ -14,4 +18,9 @@ text("dog",45,75);
 noFill();
 stroke("#FF0000");
 rect(30,60,450,350);
+fill("#FF0000");
+text("cat",320,120);
+noFil();
+stroke("#FF0000");
+rect(300,90,270,320);
 }
