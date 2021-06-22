0bc5deb0b09a4de27f581635c8c44f222030d5c3
Mahe12347 <75433421+Mahe12347@users.noreply.github.com>
2021-06-22 17:53:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img="";
 status="";
+object=[];
 function setup()
 {
 canvas=createCanvas(640,420);
@@ -16,8 +17,28 @@ function modelLoaded(){
 function preload(){
     img=loadImage('dog_cat.jpg');
 }
+function gotResult(error,results){
+if (error) {
+    console.log(error);
+}
+console.log(results);
+objects=results;
+}
 function draw(){
     image(img,0,0,640,420);
+
+    if(status !="")
+    {
+        for(i=0;i<objects.legth;i++)
+        document.getElementById("status").innerHTML="status:object Detected";
+
+        fill("#FF0000");
+        percent=floor(objects[i].confidence*100);
+        text(objects[i].label+""+percent+"%",objects[i],x+15,objects[i].y+15);
+        noFill();
+        stroke("#FF0000");
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+    }
 fill("#FF0000");
 text("dog",45,75);
 noFill();
